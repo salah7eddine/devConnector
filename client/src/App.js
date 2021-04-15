cc397dfee9cfc65b68c410fd2a998c11b25a341d
salah7eddine <abousaid7salah@gmail.com>
2021-04-15 15:19:11 +0000

    Alert Reducer, Action & Types

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,26 @@ import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
---
+++ client/src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import alert from './Alert';
+
+export default combineReducers({ alert });
