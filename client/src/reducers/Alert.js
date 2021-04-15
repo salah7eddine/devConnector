a21a448a7dd083f7c1dc2b7bb14cf71c3e3cac95
salah7eddine <abousaid7salah@gmail.com>
2021-04-15 16:36:14 +0000

    Alert Component & Action Call

--- client/src/reducers/Alert.js
+++ client/src/reducers/Alert.js
@@ -1,4 +1,4 @@
-import { SET_ALERT, REMOVE_ALERT } from '../actions/Types';
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [];
 
@@ -9,6 +9,7 @@ const alert = (state = initialState, action) => {
     case SET_ALERT:
       return [...state, payload];
     case REMOVE_ALERT:
+      console.log('REMOVE_ALERT');
       return state.filter((alert) => alert.id !== payload);
     default:
       return state;
