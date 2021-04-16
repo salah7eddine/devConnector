007fa0720d91a29d7597a3aa12cc16227b6cbd18
salah7eddine <abousaid7salah@gmail.com>
2021-04-16 15:38:44 +0000

    Profile Reducer & Get Current Profile

--- client/src/reducers/Alert.js
+++ client/src/reducers/Alert.js
@@ -9,7 +9,6 @@ const alert = (state = initialState, action) => {
     case SET_ALERT:
       return [...state, payload];
     case REMOVE_ALERT:
-      console.log('REMOVE_ALERT');
       return state.filter((alert) => alert.id !== payload);
     default:
       return state;
