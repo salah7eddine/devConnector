4787be90106fb3272677196a03a46e80f6752127
salah7eddine <abousaid7salah@gmail.com>
2021-04-20 14:32:26 +0000

    Adressing The Console Warnings

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -219,7 +219,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = (id) => async (dispatch) => {
   if (window.confirm('Are you sure ? this can NOT be undone!')) {
     try {
-      const res = await axios.delete(`api/profile`);
+      await axios.delete(`api/profile`);
 
       dispatch({
         type: CLEAR_PROFILE,
