94113d1fd158fb69949da36410adf8861bbba34a
salah7eddine <abousaid7salah@gmail.com>
2021-05-01 17:10:32 +0000

    Single Post Display

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -61,6 +62,7 @@ const App = () => {
                 component={AddExperience}
               />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
