856e8275e4e2e3cc043adf886bd02d0d22c3bf4f
salah7eddine <abousaid7salah@gmail.com>
2021-05-02 12:41:58 +0000

    Install Heroku CLI && Prepare For Deployement

--- servers.js
+++ servers.js
@@ -5,6 +5,7 @@ const users = require('./routes/api/users');
 const auth = require('./routes/api/auth');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const path = require('path');
 
 const app = express();
 
@@ -14,17 +15,21 @@ connectDB();
 // Body Parser Middleware, Init
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'API Running',
-  });
-});
-
 // Define Routes
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
